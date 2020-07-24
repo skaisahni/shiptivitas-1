428b89b7b37184e63e77208ed7784df94e841826
Sikander Nishad <skaicybertron@gmail.com>
2020-07-24 22:08:30 +0100

    changes are done

--- src/Board.js
+++ src/Board.js
@@ -21,6 +21,57 @@ export default class Board extends React.Component {
       complete: React.createRef(),
     }
   }
+
+  componentDidMount() {
+    this.drake = Dragula([
+      this.swimlanes.backlog.current,
+      this.swimlanes.inProgress.current,
+      this.swimlanes.complete.current,
+    ]);
+    this.drake.on('drop', (el, target, source, sibling) => this.updateClient(el, target, source, sibling));
+  }
+  componentWillUnmount() {this.drake.remove();
+  }
+  updateClient(el, target, _, sibling) {
+    
+    this.drake.cancel(true);
+
+    // Find out which swimlane the block  moved to
+    let targetSwimlane = 'backlog';
+    if (target === this.swimlanes.inProgress.current) {
+      targetSwimlane = 'in-progress';
+    } else if (target === this.swimlanes.complete.current) {
+      targetSwimlane = 'complete';
+    }
+
+    // Create a new array
+    const clientsList = [
+      ...this.state.clients.backlog,
+      ...this.state.clients.inProgress,
+      ...this.state.clients.complete,
+    ];
+    const clientMoved = clientsList.find(client => client.id === el.dataset.id);
+    const clientMovedClone = {
+      ...clientMoved,
+      status: targetSwimlane,
+    };
+
+    // Remove selected block from the List
+    const updatedClients = clientsList.filter(client => client.id !== clientMovedClone.id);
+
+    // Place selectedblock to target
+    const index = updatedClients.findIndex(client => sibling && client.id === sibling.dataset.id);
+    updatedClients.splice(index === -1 ? updatedClients.length : index, 0, clientMovedClone);
+
+    // Update React state 
+    this.setState({
+      clients: {
+        backlog: updatedClients.filter(client => !client.status || client.status === 'backlog'),
+        inProgress: updatedClients.filter(client => client.status && client.status === 'in-progress'),
+        complete: updatedClients.filter(client => client.status && client.status === 'complete'),
+      }
+    });
+  }
   getClients() {
     return [
       ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'in-progress'],
